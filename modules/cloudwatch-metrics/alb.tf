457657a2243c0a468d5e2f31698d728175008af1
Cory Jones <cory@Corys-MacBook-Air.local>
2026-05-27 16:16:25 -0400

    fixed sum

--- modules/cloudwatch-metrics/alb.tf
+++ modules/cloudwatch-metrics/alb.tf
@@ -2,11 +2,12 @@ resource "aws_cloudwatch_metric_alarm" "alb_metric_alarm" {
     alarm_name =  "alb-metric-alarm"
     metric_name = "HTTPCode_ELB_5XX_Count"
     namespace = "AWS/ApplicationELB"
-    statistic = "sum"
+    statistic = "Sum"
     period = 300
     evaluation_periods = 2
     threshold = 5
     comparison_operator = "GreaterThanOrEqualToThreshold"
+   
 
     dimensions = {
       Loadbalancer = var.alb_arn_suffix
